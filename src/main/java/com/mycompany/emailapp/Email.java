f697d6ffba2bb9ad028200f11ec2b35b1cf0806d
giuse <rufy333@gmail.com>
2020-09-03 00:11:22 +0200

    Added password check

--- src/main/java/com/mycompany/emailapp/Email.java
+++ src/main/java/com/mycompany/emailapp/Email.java
@@ -1,16 +1,15 @@
 package com.mycompany.emailapp;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Email {
-    private String firstName;
-    private String lastName;
+    private final String firstName;
+    private final String lastName;
     private String password;
-    private final int defaultPasswordLength = 4;
+    private final int defaultPasswordLength = 8;
     private String department;
-    private int mailBoxCapacity;
-    private String email;
-    private String alternatEmail;
+    private final String email;
     private final String companySuffix = "google.com";
     
     // Constructor to receive first and last name    
@@ -25,11 +24,22 @@ public class Email {
     // Ask for the departemnt
     private void setDepartment(){
         Scanner keyboard = new Scanner(System.in);
-        int depChoice;
+        int depChoice = 0;
+        String msg = "DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\nEnter your department code :";
         
         do{
-            System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter your department :");        
-            depChoice = keyboard.nextInt();
+            boolean isInt = false;
+
+            do{
+               System.out.println(msg);  
+               if(keyboard.hasNextInt()){
+                    depChoice = keyboard.nextInt();
+                    isInt = true;
+                }else{
+                    keyboard.nextLine();
+                    System.out.println("Enter a valid Integer value");
+                }
+            } while (!isInt);
 
             switch (depChoice) {
                 case 1:
@@ -41,52 +51,128 @@ public class Email {
                 case 3:
                     this.department = "accounting";
                     break;
-                default:
-                    this.department = "";
-                    break;
             }
         }
-        while(depChoice < 0 || depChoice > 3);
+        while(depChoice <= 0 || depChoice > 3);
     }
     
     // Generate a random password
     private void setRandomPassword(int length){
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$";
-        String password = "";
+        String tmpPassword = "";
         
         for (int i = 0; i < length ; i ++){
            int rand = (int)(Math.random() * passwordSet.length()) ;
-           password += passwordSet.charAt(rand);
+           tmpPassword += passwordSet.charAt(rand);
         }
-        this.password = password;
+        this.password = tmpPassword;
     }
-    
-    // Set mailbox capacity
-    public void setMailBoxCapacity(int mailBoxCapacity) {
-        this.mailBoxCapacity = mailBoxCapacity;
-    }
-
-    // Set alternate email
-    public void setAlternatEmail(String alternatEmail) {
-        this.alternatEmail = alternatEmail;
-    }   
     
     // Change the password   
-    public void changePassword(String password) {
-        this.password = password;
+    public void changePassword() {
+        Scanner keyboard = new Scanner(System.in);
+        String tmpPassword = "";
+        
+        boolean hasSpecialChars;
+        boolean hasNumbers;
+        boolean hasLetters;
+        boolean hasMinLength;
+        
+        do{
+            System.out.println("Insert your new password:");
+            try {
+                tmpPassword = keyboard.nextLine();                
+            } catch (InputMismatchException e) {
+                System.err.println("Error!");
+            }
+            
+            // Check if the new passowrd contains letters, numbers and special characthers
+            hasMinLength = pwdHasMinLenght(tmpPassword);
+            hasSpecialChars = pwdHasSpecialChars(tmpPassword);
+            hasNumbers = pwdHasNumbers(tmpPassword);
+            hasLetters = pwdHasLetters(tmpPassword);
+            
+            if (!(hasMinLength && hasSpecialChars && hasNumbers && hasLetters))
+                System.out.println("You must have letters, numbers and special characters and at least 8 digits (max digits 14)");
+            }
+        
+        while(!(hasMinLength && hasSpecialChars && hasNumbers && hasLetters));
+        
+        this.password = tmpPassword;     
+        
+        System.out.println("Password has been changed successfully!");
+        
+    }    
+    
+    private boolean pwdHasMinLenght(String password){
+        return (password.length() >= 8 && password.length() <=  14);
     }
+    
+    private boolean pwdHasSpecialChars(String password){
+        String[] specialChars = {"?","!","@","#","$"};
+        boolean result = false;
+        for (String specialChar : specialChars){
+            if (password.contains(specialChar)){
+                result = true;
+                break;
+            }
+        }
+        
+        return result;
+    }    
+    
+    private boolean pwdHasNumbers(String password){        
+//        return password.matches(".*\\d*.");
+        boolean result = false;
+        final String [] numbers = {"0","1","2","3","4","5","6","7","8","9"};
+        
+        for (String number : numbers){
+            if (password.contains(number)){
+                result = true;
+                break;
+            }        
+        }
+        
+        return result;
+    }
+    
+    private boolean pwdHasLetters(String password){        
+        char[] alphabet = getAlphabet();
+        boolean result = false;
+        for (int i = 0; i < alphabet.length; i++){
+           if( password.indexOf(alphabet[i]) != -1){
+               result = true;
+               break;               
+           }
+        }
+       
+        return result;
+    }    
+    
+    private char[] getAlphabet(){
+        char[] charLetters = new char[122-97+1];
+        int tmp = 97;
+        for (int i = 0; i < charLetters.length; i++){
+                charLetters[i] = (char) tmp;
+                tmp++;
+            }        
+        
+        return charLetters;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
 
     @Override
     public String toString() {
-        return "Email{" + "firstName=" + firstName + ", lastName=" + lastName + ", password=" + password + ", department=" + department + ", mailBoxCapacity=" + mailBoxCapacity + ", email=" + email + '}';
+        return "Email{" + "firstName=" + firstName + ", lastName=" + lastName + ", password=" + password + ", department=" + department + ", email=" + email + '}';
     }
-
-    
-
-    
-
-    
-    
-    
     
 }
--- src/main/java/com/mycompany/emailapp/EmailApp.java
+++ src/main/java/com/mycompany/emailapp/EmailApp.java
@@ -2,13 +2,15 @@ package com.mycompany.emailapp;
 
 public class EmailApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {         
+        Email email1 = new Email("John","Smith");   
+        System.out.println("Your email based on your first name, last name and department: " + email1.getEmail());        
+        System.out.println(email1);        
+        System.out.println("----------------------------------");        
         
-        Email email1 = new Email("John","Smith");
-        
-        
+        email1.changePassword();
+        System.out.println("Your new password is: " + email1.getPassword());
         System.out.println(email1);
-        
     }
     
 }
